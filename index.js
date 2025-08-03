e37c320307ca19a6b501f230ff0fa9fb146a15b9
Tanty2x <tanhoang2x@gmail.com>
2025-08-03 14:51:30 +0700

    sửa đổi thêm 1 ít header cho có

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import statusRouter from "./routes/status.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use("/api", statusRouter);
 
-app.get("/api", (req, res) => {
-  res.send("Hello World!");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, () => {
